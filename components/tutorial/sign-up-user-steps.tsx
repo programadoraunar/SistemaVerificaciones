1293db24b42e28fab484d8ced4e5d06288d4addf
Programador <programador@aunar.edu.co>
2024-09-10 09:19:02 -0500

    Subiendo Proyecto

--- components/tutorial/sign-up-user-steps.tsx
+++ components/tutorial/sign-up-user-steps.tsx
@@ -70,19 +70,6 @@ export default function SignUpUserSteps() {
           </Link>
         </TutorialStep>
       ) : null}
-      <TutorialStep title="Sign up your first user">
-        <p>
-          Head over to the{" "}
-          <Link
-            href="/sign-up"
-            className="font-bold hover:underline text-foreground/80"
-          >
-            Sign up
-          </Link>{" "}
-          page and sign up your first user. It's okay if this is just you for
-          now. Your awesome idea will have plenty of users later!
-        </p>
-      </TutorialStep>
     </ol>
   );
 }

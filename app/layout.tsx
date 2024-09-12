573bb4a0acfd03142d6be6528ac058be96e82c34
Programador <programador@aunar.edu.co>
2024-09-12 17:54:31 -0500

    change on title

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Sistema de Validacion de Certificados Institucionales",
+  description: "AUNAR",
 };
 
 export default function RootLayout({
